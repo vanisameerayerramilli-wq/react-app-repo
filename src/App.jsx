7c5ef896b6d833cfca734056becbd50f9cda8e75
vanisameerayerramilli-wq <vanisameerayerramilli@gmail.com>
2025-09-14 19:50:06 +0530

    Updated Website name

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import './App.css';
+import Footer from './components/footer';
+import Header from './components/header';
+import LandingPage from './components/landingpage';
+
+function App() {
+  return (
+    <div className="App">
+        <Header />
+        <LandingPage />
+        <Footer/>
+    </div>
+  );
+}
+
+export default App;
